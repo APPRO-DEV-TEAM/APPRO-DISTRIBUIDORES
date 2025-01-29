7dccee61b7edab77c7ca356d49e2f4a652ac3d90
Henrique Costa <henrique.costa@flowih.com>
2025-01-29 19:27:00 -0300

    feat: add Google Maps autocomplete functionality and utility class name merger

---
+++ src/lib/google.ts
@@ -0,0 +1,19 @@
+import { Client } from "@googlemaps/google-maps-services-js";
+
+const client = new Client();
+export const autocomplete = async (input: string) => {
+  if (!input) return [];
+
+  try {
+    const response = await client.placeAutocomplete({
+      params: {
+        input,
+        key: "AIzaSyATFFlBVvbstEAytcAChHNX73TIrsFmGzU",
+      },
+    });
+
+    return response.data.predictions;
+  } catch (error) {
+    console.error(error);
+  }
+};

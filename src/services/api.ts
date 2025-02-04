07c163a2bd558b361fbc33312710632a1fb8fbcd
Henrique Costa <henrique.costa@flowih.com>
2025-02-04 20:03:14 -0300

    feat: update API base URL to use HTTPS for improved security

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://api.appro.com.br:3333/api/",
+  baseURL: "https://api.appro.com.br/api/",
   timeout: 10000,
   timeoutErrorMessage:
     "A requisição demorou muito para responder. Tente novamente mais tarde.",
